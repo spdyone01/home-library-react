374c5d51bc78b66b160afb39c1f95572a09e7efb
DarkNesce <DarkNesce@users.noreply.github.com>
2021-10-19 15:12:00 -0600

    added back link to registration page

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Registration(props) {
   return (
     <div className='registration-container'>
+      <Link to='/'>Back</Link>
       <p>Registration Component</p>
     </div>
   );
