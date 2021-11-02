d3f2db890c2bea183d0a621f5285e023c24e5474
DarkNesce <DarkNesce@users.noreply.github.com>
2021-11-01 18:21:40 -0600

    add Title, add plugin transform runtime for async

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,27 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+// import { updateSearchQuery, updateResults } from "../actions/search";  // These may be deprecated in favor of state in this component
 import { getSearchResults } from '../api/openlibraryapi';
-import SearchBar from "./SearchBar";
 
-function AddBook(props) {
-  let searchResults = [];
-  const searchBarEl = document.getElementById('#search-bar');
-  console.log(searchBarEl)
+const SearchPage = (props) => {
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const [results, setSearchResults] = useState({});
+
+  const searchSubmit = async () => {
+    try{
+      const results = await getSearchResults(searchQuery, { searchType: `${props.filters.sortBy}`});
+      console.log(results);
+      // handleSearchResults(results);  // create something that will test for results > 0 and initialize default data for addbook form
+      setSearchQuery('');
+      // create cards with handled search results - use showcase arrows for images (up to 10?)
+    } catch (error) {
+      console.log("error", error);
+      setSearchQuery('');
+    }
+  }
 
   React.useEffect(() => {
-    getSearchResults('harry potter', { searchType: 'title'});
+
   }, []);
-
-
+  
   let message = 'Search for a book above!';
   let loading = false;
 
   return (
     <div className='search-page-container'>
-      <SearchBar placeholder='Search for a book'/>
+    
+      <div className='search-bar'>
+        <form className='search-form'
+          onSubmit={(e) => {
+            e.preventDefault();
+            searchSubmit(searchQuery, setSearchQuery);
+          }}>
+          <input
+              id='search-query'
+              type='text'
+              placeholder={'Search for a book'}
+              value={searchQuery}
+              onChange={(e) => {
+                setSearchQuery(e.target.value);
+              }}
+              
+          />
+          <input type='image' id='search-button' src='../media/book-svgrepo-com.svg' alt='search button' width='20px'></input>
+        </form>
+      </div>
+
       <div className='search-results'>
         {
-          (searchResults.length !== 0) ?
-          {searchResults}
+          (0 !== 0) ?
+          <p>Change me later</p>
           :
           <p className='message'>
             {
@@ -36,4 +69,11 @@ function AddBook(props) {
   );
 }
 
-export default AddBook;
+const mapStateToProps = (state) => {
+  return {
+      filters: state.filters,
+      search: state.search
+  };
+};
+
+export default connect(mapStateToProps)(SearchPage);
