6eea3e30ef352958a4933c49773635df09adc88b
DarkNesce <DarkNesce@users.noreply.github.com>
2022-04-12 11:05:21 -0600

    missing () in actions, update build2 to deploy gh

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "public"),
+    path: path.join(__dirname, "/public"),
     filename: "bundle.js",
   },
   mode: "development",
