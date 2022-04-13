757f66f0c6ef9957ee0423204991240d15da98e4
DarkNesce <DarkNesce@users.noreply.github.com>
2022-04-13 14:13:19 -0600

    Get rid of faint border around navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className="w-full row-span-1 place-content-center h-20 relative navbar self-end text-xs mx-0 px-0">
-      <div className="flex-none w-full h-full place-content-center border-2 space-between rounded-xl max-w-2xl">
+      <div className="flex-none w-full h-full place-content-center space-between rounded-xl max-w-2xl">
         <ul className="menu menu-horizontal m-0 p-0 max-w-lg justify-between text-slate-800 text-center">
           <li className="hover:bg-slate-500 hover:text-zinc-900 rounded-xl w-1/5">
               <Link className="w-full place-content-center" to="/home">Home</Link>
